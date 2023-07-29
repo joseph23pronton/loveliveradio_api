8a6c3084509c9405db4dbf56093fbb8c3b805454
Joseph Pronton <joseph23pronton@gmail.com>
2023-07-29 22:45:37 +0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import cors from 'cors';
+import http from "http";
 
 import { resolvers } from "./api/resolvers.js";
 import { typeDefs } from "./api/schemas.js";
@@ -16,7 +18,25 @@ await server.applyMiddleware( { app } )
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(express.json());
+
+const httpServer = http.createServer(app);
 
 app.listen({ port: PORT }, () => {
     console.log(`Server is running at http://localhost:${PORT}${server.graphqlPath}`)
 })
+
+const startApolloServer = async(app, httpServer) => {
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+  
+    await server.start();
+    server.applyMiddleware({ app });
+  }
+
+  startApolloServer(app, httpServer);
+
+export default httpServer;
