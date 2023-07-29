620b82cdd63ef130c1f1378399616b3d5e802bf6
Joseph Pronton <joseph23pronton@gmail.com>
2023-07-29 23:28:18 +0800

    teasfasfw

--- api/index.js
+++ api/index.js
@@ -1,38 +1,37 @@
-import express from 'express';
-import bodyParser from "body-parser";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, gql } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import cors from 'cors';
 import http from "http";
-
-import { resolvers } from "./resolvers.js";
-import { typeDefs } from "./schemas.js";
-import { PORT } from "../config/config.js";
+import express from "express";
+import cors from "cors";
 
 const app = express();
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(express.json());
-
 const httpServer = http.createServer(app);
 
-const startApolloServer = async (app, httpServer) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-  await server.start();
-  server.applyMiddleware({ app });
-
-  httpServer.listen({ port: PORT }, () => {
-    console.log(`Server is running at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
 };
 
-startApolloServer(app, httpServer);
+const startApolloServer = async(app, httpServer) => {
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+  
+    await server.start();
+    server.applyMiddleware({ app });
+  }
+
+  startApolloServer(app, httpServer);
 
 export default httpServer;
