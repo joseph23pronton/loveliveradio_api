64284ede505b9e6e51bd46a9f8499e7daf96890a
joseph23pronton <67457574+joseph23pronton@users.noreply.github.com>
2023-07-29 22:23:21 +0800

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// config.js
+const ENV = {
+    mongoURL: "mongodb+srv://raburadio:ganbaruby23@killerspit23db.ybnim.gcp.mongodb.net/?retryWrites=true&w=majority",
+  };
+  
+  export { ENV };
+  // config.js
+const PORT = 3001; // Replace 3000 with your desired port number
+
+export { PORT };
+  
