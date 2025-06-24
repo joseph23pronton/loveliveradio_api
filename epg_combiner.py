6db40a666c2f0a347a58d033f1b1b9322c0f1fef
GanbaRuby23 <67457574+joseph23pronton@users.noreply.github.com>
2025-06-25 06:12:39 +0800

    Update epg_combiner.py

--- epg_combiner.py
+++ epg_combiner.py
@@ -10,7 +10,7 @@ EPG_URLS = [
     "https://raw.githubusercontent.com/atone77721/CIGNAL_EPG/refs/heads/main/sky_epg.xml",
     "https://raw.githubusercontent.com/pigzillaaaaa/blast-epg/refs/heads/main/blast-epg.xml",
     "https://raw.githubusercontent.com/atone77721/CIGNAL_EPG/refs/heads/main/cignal_epg.xml",
-    "https://kaotv.ganbaruby23.xyz/epg.xml",
+    "https://tv.ganbaruby23.xyz/xmltv/channels",
 ]
 COMBINED_EPG_FILE = "combined_epg.xml"
 REQUEST_TIMEOUT = 15  # seconds
